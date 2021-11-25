5aa4194c3bd2a954b186969ea0bf91b53d65cb89
rohansawant7978 <90745010+rohansawant7978@users.noreply.github.com>
2021-11-25 20:30:16 +0530

    Update gec_app.py

--- app/gec_app.py
+++ app/gec_app.py
@@ -4,5 +4,9 @@ st.title('Grammar Error Correction')
 st.header('Using Attention Mechanism')
 st.markdown('created by: **Rohan Sawant**')
 
-st.text_area("Enter Text", '')
-st.button('Check Your Text')
+inp = st.text_area("Enter Text", '')
+submit = st.button('Check Your Text')
+
+if submit:
+    actual = function1(inp)
+    st.text(f'last refreshed on {datetime.now(timezone("Asia/Kolkata")).strftime("%Y-%m-%d %I:%M:%S %p")}')
